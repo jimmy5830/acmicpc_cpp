280fdc5097661054f027ee84caa4441812d2cb22
jimmy5830 <jimmy5830@khu.ac.kr>
2026-01-28 11:55:10 +0900

    1.28

---
+++ 12891.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    queue<int> Q;
+    int N;
+    
+    if (!(cin >> N)) return 0;
+    
+    for (int i = 1; i <= N; i++) {
+        Q.push(i);
+    }
+    
+    while (Q.size() > 1) {
+        Q.pop();
+        Q.push(Q.front());
+        Q.pop();
+    }
+    
+    if (!Q.empty()) {
+        cout << Q.front();
+    }
+    
+    return 0;
+}
